0b5cf08e0b41d5639965d38e5515ecf2b7b2a926
Bassim2023 <136567954+Bassim2023@users.noreply.github.com>
2023-09-21 09:40:21 +0200

    Update workshop2.sql

--- workshop2.sql
+++ workshop2.sql
@@ -2,79 +2,94 @@
 #
 #
 # 1: Get the cities with a name starting with ping sorted by their population with the least populated cities first
-select * from city 
+select * 
+from city 
 where name like "ping%"
 order by population; 
 #
 # 2: Get the cities with a name starting with ran sorted by their population with the most populated cities first
-select * from city
+select * 
+from city
 where name like "ran%"
 order by population desc;
 #
 #
 # 3: Count all cities
-select count(*) from city;
+select count(*) 
+from city;
 #
 #
 # 4: Get the average population of all cities
-select avg(Population) from city; 
+select avg(Population) 
+from city; 
 #
 #
 # 5: Get the biggest population found in any of the cities
-select max(Population) from city; 
+select max(Population) 
+from city; 
 #
 # 6: Get the smallest population found in any of the cities
-select  min(Population) from city; 
+select  min(Population) 
+from city; 
 #
 # 7: Sum the population of all cities with a population below 10000
-select sum(population) from city
+select sum(population) 
+from city
 where population < 10000;
 #
 # 8: Count the cities with the countrycodes MOZ and VNM
-select count(*) from city
+select count(*) 
+from city
 where countrycode in ("MOZ", "VNM");
 #
 # 9: Get individual count of cities for the countrycodes MOZ and VNM
-select countrycode, count(*) from city
+select countrycode, count(*) 
+from city
 where countrycode in ("MOZ", "VNM")
 group by countrycode;
 
 #
 #
 # 10: Get average population of cities in MOZ and VNM
-select countrycode, avg(population) from city
+select countrycode, avg(population) 
+from city
 where countrycode in ("MOZ", "VNM")
 group by countrycode;
 #
 #
 # 11: Get the countrycodes with more than 200 cities
-select countrycode, count(*) from city
+select countrycode, count(*) 
+from city
 group by countrycode
 having count(*) > 200;
 
 #
 #
 # 12: Get the countrycodes with more than 200 cities ordered by city count
-select countrycode, count(*) from city
+select countrycode, count(*) 
+from city
 group by countrycode
 having count(*) > 200
 order by count(*);
 
 #
 # 13: What language(s) is spoken in the city with a population between 400 and 500 ?
-select distinct countrylanguage.Language, city.name  from city
+select distinct countrylanguage.Language, city.name  
+from city
 join countrylanguage on city.CountryCode = countrylanguage.CountryCode
 where city.population between 400 and 500;
 
 #
 # 14: What are the name(s) of the cities with a population between 500 and 600 people and the language(s) spoken in them
 
-select distinct countrylanguage.Language, city.name  from city
+select distinct countrylanguage.Language, city.name  
+from city
 join countrylanguage on city.CountryCode = countrylanguage.CountryCode
 where city.population between 500 and 600;
 #
 # 15: What names of the cities are in the same country as the city with a population of 122199 (including the that city itself)
-select name from city
+select name 
+from city
 where CountryCode = (select CountryCode from city where Population = 122199);
 
 SELECT City.Name AS CityName, City.CountryCode, city.Population AS CountryCode
